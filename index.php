5cf1be2d7271bd7bb1de83bf8e5657058b77de29
ankitapadalkar <ankitapadalkar2020@gmail.com>
2024-07-03 10:59:19 +0530

    leave-management-system

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include "templates/header.php;"?>
+
+<section class="hero-banner d-flex align-item-center justify-content-center">
+<div class="container text-center text-black">
+    <h1>Leave Management System-PHP</h1>
+    <p>my college</p>
+    <button class="btn-primary">Login</button>
+</div>
+</section>
+
+
+<?php
+    include 'templetes/footer.php';>
